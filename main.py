9303dd08bedad1f2378f071069d7af58660fc481
Nakohartum <artemofgta@gmail.com>
2025-09-08 10:02:43 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(a, b):
+    # Use a breakpoint in the code line below to debug your script.
+    return a + b
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print_hi(a, b)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
